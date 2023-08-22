2922383ca54f33696dfda8b7582a530d2637c073
hotnAny <xiangchen@acm.org>
2023-08-22 11:16:15 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,203 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useRef } from 'react';
+import yaml from 'js-yaml';
+import AceEditor from 'react-ace'
+import 'ace-builds/src-noconflict/theme-github';
+import Vitae from './Vitae';
+
+const categories = {
+  'Basic Information': [],
+  'Current Position': [],
+  'Education': [],
+  'Awards': [],
+  'Professional Experiences': [],
+  'Publications': ['Conferences & Journals', 'Dissertations & These', 'Book Chapters', 'Magazine Articles']
+}
 
 function App() {
+  const [category, setCategory] = useState('');
+  const [subCategory, setSubCategory] = useState('');
+  const [subCategoryOptions, setSubCategoryOptions] = useState([]);
+
+  const [startYear, setStartYear] = useState('');
+  const [endYear, setEndYear] = useState('');
+  const [label, setLabel] = useState('');
+  const [description, setDescription] = useState('');
+  const [link, setLink] = useState('');
+
+  const [entries, setEntries] = useState({});
+  const [isDataReady, setDataReady] = useState(false);
+
+  const editorRef = useRef(null);
+  const [statusText, setStatusText] = useState('...');
+
+  // const vitaeRef = useRef(null);
+
+  const handleStartYearChange = (e) => {
+    setStartYear(e.target.value);
+  };
+
+  const handleEndYearChange = (e) => {
+    setEndYear(e.target.value);
+  };
+
+  const handleCategoryChange = (e) => {
+    const selectedValue = e.target.value;
+    setCategory(selectedValue);
+    setSubCategory('');
+    setSubCategoryOptions(categories[selectedValue]);
+  };
+
+  const handleSubCategoryChange = (e) => {
+    setSubCategory(e.target.value);
+  };
+
+  const handleLabelChange = (e) => {
+    setLabel(e.target.value);
+  };
+
+  const handleDescriptionChange = (e) => {
+    setDescription(e.target.value);
+  };
+
+  const handleLinkChange = (e) => {
+    setLink(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    // localStorage.clear();
+
+    e.preventDefault();
+    const entry = { startYear, endYear, label, description, link };
+
+    syncData();
+
+    if (!entries[category]) {
+      entries[category] = {}
+    }
+
+    if (!entries[category][subCategory]) {
+      entries[category][subCategory] = []
+    }
+    entries[category][subCategory].push(entry)
+
+    const rawDataNew = yaml.dump(entries);
+    syncData(rawDataNew);
+
+    // update the editor
+    editorRef.current.editor.setValue(rawDataNew)
+
+  };
+
+  const handleSave = () => {
+    const rawDataNew = editorRef.current.editor.getValue();
+
+    syncData(rawDataNew);
+
+    setStatusText("Data saved.");
+    setTimeout(() => {
+      // TODO: find a better placeholder
+      setStatusText("...");
+    }, 1000);
+  };
+
+  //
+  // synchronize data across local storage, the editor view, and the PDF view
+  //
+  const syncData = (rawDataNew) => {
+    // signal to the PDF viewer that data is not ready 
+    setDataReady(false)
+
+    // in case new data needs to be updated to local storage
+    if (rawDataNew) {
+      localStorage.setItem('entries', rawDataNew);
+    }
+
+    const localStorageData = localStorage.getItem('entries');
+    if (localStorageData) {
+      setEntries(yaml.load(localStorageData));
+    }
+
+    console.log(entries);
+
+    // signal to the PDF viewer that data is now ready 
+    setDataReady(true);
+  }
+
+  useEffect(() => {
+    syncData()
+
+    // initialize the editor
+    const editor = editorRef.current.editor;
+
+    // register the 'Cmd-S' command
+    editor.commands.addCommand({
+      name: 'save',
+      bindKey: { win: 'Ctrl-S', mac: 'Cmd-S' },
+      exec: handleSave,
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <table>
+        <tr>
+          <td colSpan="2">
+            <h1>OpenVitae</h1>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <div>
+              <form onSubmit={handleSubmit}>
+                <select value={category} onChange={handleCategoryChange}>
+                  <option value="">-- Category --</option>
+                  <option value="Basic Information">Basic Information</option>
+                  <option value="Current Position">Current Position</option>
+                  <option value="Education">Education</option>
+                  <option value="Professional Experiences">Professional Experiences</option>
+                  <option value="Awards">Awards</option>
+                  <option value="Publications">Publications</option>
+                </select>
+
+                <select value={subCategory} onChange={handleSubCategoryChange}>
+                  <option value="">-- Select --</option>
+                  {subCategoryOptions.map((option, index) => (
+                    <option key={index} value={option}>
+                      {option}
+                    </option>
+                  ))}
+                </select>
+
+                <input type="number" value={startYear} onChange={handleStartYearChange} placeholder='Start Year' />
+                <input type="number" value={endYear} onChange={handleEndYearChange} placeholder='End Year' />
+
+                <br />
+
+                <input type="text" value={label} onChange={handleLabelChange} placeholder='Label' />
+                <input type="text" value={description} onChange={handleDescriptionChange} placeholder='Description' />
+
+                <br />
+
+                <input type="text" value={link} onChange={handleLinkChange} placeholder='Link' />
+
+                <button type="submit">Submit</button>
+              </form>
+
+              <div>{statusText}</div>
+              <AceEditor
+                ref={editorRef}
+                theme="github"
+                mode="yaml"
+                value={yaml.dump(entries)}
+              />
+
+            </div>
+          </td>
+          <td >
+            {isDataReady ? <Vitae entries={entries} /> : <p>Loading ...</p>}
+          </td>
+        </tr>
+      </table>
     </div>
   );
 }
---
+++ src/Vitae.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState, useRef } from 'react';
+import { PDFViewer } from '@react-pdf/renderer';
+import { Document, Page, Text, View, StyleSheet } from '@react-pdf/renderer';
+
+function Vitae(data) {
+    const entryBlank = { startYear: '', endYear: '', label: '', description: '', link: '' };
+    const [currentPosition, setCurrentPosition] = useState(entryBlank)
+
+    useEffect(() => {
+        const entries = data.entries;
+
+        if (entries) {
+            if (entries['Current Position']) {
+                const objsCurrentPosition = entries['Current Position']['']
+                if (objsCurrentPosition && objsCurrentPosition.length > 0) {
+                    setCurrentPosition(objsCurrentPosition[0])
+                }
+            }
+        }
+
+    }, []);
+
+    const styles = StyleSheet.create({
+        page: {
+            flexDirection: 'row',
+            backgroundColor: '#E4E4E4'
+        },
+        section: {
+            margin: 10,
+            padding: 10,
+            flexGrow: 1
+        }
+    });
+
+    const VitaePage = () => (
+        <Document>
+            <Page size="A4" style={styles.page}>
+                <View style={styles.section}>
+                    <Text>Current Position</Text>
+                    <Text>{currentPosition.startYear}</Text>
+                </View>
+                <View style={styles.section}>
+                    <Text>Section #2</Text>
+                </View>
+            </Page>
+        </Document>
+    );
+
+    return (
+        <PDFViewer>
+            <VitaePage />
+        </PDFViewer>
+    );
+}
+
+export default Vitae;
